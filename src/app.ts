13ec7a5a26d6fc428e70a8f6133f2b9d164cdb56
ardianilyas <ardianilyas@gmail.com>
2025-09-10 00:02:26 +0700

    feat: update app.ts endpoint add auth routes

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,28 @@
 import dotenv from "dotenv";
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoute from "./modules/auth/auth.route";
+import { requireAuth } from "./middlewares/auth";
 
 dotenv.config();
 
 const app: Application = express();
 
-app.use(cors())
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
     res.json({ 
         message: "Welcome to kyoo event management and ticekting system"
     });
 }); 
+
+app.use("/api/auth", authRoute);
+
+app.get("/api/me", requireAuth, (req, res) => {
+    res.json({ user: req.user });
+})
 
 export default app;
