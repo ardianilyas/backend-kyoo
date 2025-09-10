4beeffeb2b673c7445b90235752706ef14b980a4
ardianilyas <ardianilyas@gmail.com>
2025-09-10 18:53:41 +0700

    feat: update error handler middleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,9 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 
 export function erroHandler(err: any, req: Request, res: Response, next: NextFunction) {
-    if (err.errors) {
+    if (err.name === "ZodError") {
         return res.status(err.status || 400).json({ errors: err.errors });
     }
 
-    return res.status(err.status || 500).json({ error: err.message || "Internal server error" });
+    if (err.status) {
+        return res.status(err.status).json({
+            error: err.message,
+            details: err.details || null,
+        });
+    }
+
+    return res.status(500).json({ error: err.message || "Internal server error" });
 }
