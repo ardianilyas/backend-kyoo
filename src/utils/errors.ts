1c979582385bf62f0d23e634a7227c7cb161fc90
ardianilyas <ardianilyas@gmail.com>
2025-09-11 11:04:34 +0700

    feat: update error utils

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,11 +1,46 @@
-export class NotFoundError extends Error {
+export class AppError extends Error {
     status: number;
     details: Record<string, any>;
 
-    constructor(message: string, details?: Record<string, any>) {
+    constructor(message: string, status: number,  details?: Record<string, any>) {
         super(message);
-        this.name = "NotFoundError";
-        this.status = 404;
+        this.name = "AppError";
+        this.status = status;
         this.details = details || {};
+    }
+}
+
+export class NotFoundError extends AppError {
+    constructor(message: string, details?: Record<string, any>) {
+        super(message, 404, details);
+        this.name = "NotFoundError";
+    }
+}
+
+export class ConflictError extends AppError {
+    constructor(message: string, details?: Record<string, any>) {
+        super(message, 409, details);
+        this.name = "ConflictError";
+    }
+}
+
+export class UnauthorizedError extends AppError {
+    constructor(message: string, details?: Record<string, any>) {
+        super(message, 401, details);
+        this.name = "UnauthorizedError";
+    }
+}
+
+export class ForbiddenError extends AppError {
+    constructor(message: string, details?: Record<string, any>) {
+        super(message, 403, details);
+        this.name = "ForbiddenError";
+    }
+}
+
+export class BadRequestError extends AppError {
+    constructor(message: string, details?: Record<string, any>) {
+        super(message, 400, details);
+        this.name = "BadRequestError";
     }
 }
