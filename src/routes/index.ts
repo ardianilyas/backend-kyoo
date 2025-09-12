227c00ec4a0b4d6e13ed4165381964012ca02c48
ardianilyas <ardianilyas@gmail.com>
2025-09-12 20:25:25 +0700

    feat: update routes add event route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authRoute from "../modules/auth/auth.route";
 import eventCategoriesRoute from "../modules/event-category/eventCategory.route";
 import userRoute from "../modules/user/user.route";
+import eventRoute from "../modules/event/event.route";
 
 const router = Router();
 
@@ -10,6 +11,7 @@ const apiRouter = Router();
 apiRouter.use("/auth", authRoute);
 apiRouter.use("/event-categories", eventCategoriesRoute);
 apiRouter.use("/users", userRoute);
+apiRouter.use("/events", eventRoute);
 
 router.use("/api", apiRouter);
 
